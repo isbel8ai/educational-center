01d195795c25ad37e5a2d1fc0a81aeffb8fba9f8
max <isbel8ai@aleph.engineering>
2019-02-14 18:09:08 -0500

    Remove data validation from constructor on Question class

--- school/question.py
+++ school/question.py
@@ -1,17 +1,7 @@
 class Question:
     def __init__(self, statement, choices, answer):
-        if not isinstance(statement, str) or statement == "":
-            raise ValueError()
         self.subject = statement
-
-        for choice in choices:
-            if not isinstance(choice, str) or choice == "":
-                raise ValueError()
         # choices is a list of strings of each possible choice
         self.choices = choices
-
-        for check in answer:
-            if not isinstance(check, bool):
-                raise ValueError()
         # answer is a list of boolean with same size of choices indicating if respective choice should be checked
         self.answer = answer
