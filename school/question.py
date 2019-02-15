12ca7a74fe63b6e8ff57f425b676216743a32638
max <isbel8ai@aleph.engineering>
2019-02-14 19:28:56 -0500

    Rename attribute subject to statements on class Question

--- school/question.py
+++ school/question.py
@@ -1,6 +1,6 @@
 class Question:
     def __init__(self, statement, choices, answer):
-        self.subject = statement
+        self.statement = statement
         # choices is a list of strings of each possible choice
         self.choices = choices
         # answer is a list of boolean with same size of choices indicating if respective choice should be checked
